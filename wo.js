c92047b5791921f72bbe5aeb152e7b0bab5fe6f3
jqroom <jqroom.com@gmail.com>
2017-02-22 10:24:20 +0800

    修复attr方法bug，增加遍历函数，完善初始化

--- wo.js
+++ wo.js
@@ -31,24 +31,45 @@
 			return  exec ? exec[1].toLowerCase(): typeof value;
 		},
 		isPlainObject: (value) => T.type(value) == "object",
+		isNumber: (value) => T.type(value) == "number",
 		isObject: (value) => typeof value == "object",
 		isArray: (value) => T.type(value) == "array",
-		isElemNode: (value) => !!value && typeof value == "object" && value.nodeType == 1
+		isElemNode: (value) => !!value && typeof value == "object" && value.nodeType == 1,
+		toArray: (arr) => Array.from(arr),
+		likeArray(arr){
+			if(T.isArray(arr) == "array") return arr;
+			return T.isObject(arr) && (arr.length === 0 || (T.isNumber(arr.length) && arr.length > 0 && !!arr[arr.length - 1]))
+		}
 	}
 	var arrPro   = Array.prototype,
 		forEach  = arrPro.forEach,
+		map      = arrPro.map,
+		filter   = arrPro.filter,
 		push     = arrPro.push;
 	var W = function(selector, context){
 		return Wo.init(selector, context);
 	}
+	W.map    = (dom, fn) => map.call(dom, fn);
+	W.each   = (dom, fn) => forEach.call(dom, fn);
+	W.filter = (dom, fn) => filter.call(dom, fn);
+
 	W.fn = {
-		map(){},
+		map(fn){
+			return W(W.map(this, fn));
+		},
 		each(fn){
-			return forEach.call(this, fn);
+			return W(W.each(this, fn));
+		},
+		filter(fn){
+			return W(W.filter(this, fn));
 		},
 		children(selector){
 			var dom = [];
+			this.each(elem => {
+				W(elem.childNodes).map(function(elem){
 
+				})
+			})
 		},
 		find(selector){
 			var type = T.type(selector);
@@ -73,15 +94,21 @@
 			})
 			return this;
 		},
+		remove(){
+
+		},
 		attr(key, value){
 			var attribute = (elem, key, value) =>{
-				if (!elem || elem.nodeType != 1) return;
+				if (!T.isElemNode(elem)) return;
 				if (!value) {
-					elem.getAttribute(key)
+					return elem.getAttribute(key);
 				} else {
 					elem.setAttribute(key, value);
 				}
 			} 
+			if (!value) {
+				return attribute(this[0], key);
+			}
 			this.each(function(elem, i){
 				attribute(elem, key, value);
 			});
@@ -98,16 +125,19 @@
 					this.flagElement(selector, context)
 			    else 
 					dom = this.qsa(selector.trim(), context)
-			}  else if(type == "array") {
-				dom = selector.map(function(elem){
+			}  else if(T.likeArray(selector)) {
+
+				dom = T.toArray(selector).filter(function(elem){
 					if (T.isElemNode(elem)){
 						return elem;
 					} else if(T.type(elem) == "string"){
 						return W.qsa(elem)
-					} 
+					} else {
+						return false;
+					}
 				});
-			}  else if(typeof selector == "object" && selector.nodeType == 1) {
-
+			}  else if(T.isElemNode(selector)) {         //如果为dom节点
+				dom = selector;
 			}
 			if (!dom) dom = []
 			else if (dom.length == null)  dom = [dom] 
@@ -163,8 +193,9 @@
 	
 	window.$ = window.$ || W; 
 
-	//log( Wo.matches(document.getElementsByClassName("k")[0] , ".i") );
-	log(W("#child, #parent").addClass("k"));
+	log(W("#parent, #child").filter(function(elem){
+		return W(elem).attr("cid") > 20
+	}));
 
 
 }) )
